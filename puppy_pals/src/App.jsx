d1a7ef051d8e98e04f095feffa1acc85fc4f9a50
Jimmy Signature <jimmysignature@gmail.com>
2023-06-28 22:00:37 -0400

    add feature pup

--- puppy_pals/src/App.jsx
+++ puppy_pals/src/App.jsx
@@ -4,13 +4,30 @@ import { useState } from 'react'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
-  console.log(puppies)
+  const [featPupId, setFeatPupId] = useState(null)
+  function handleClick(id) {
+    setFeatPupId(id)
+  }
+  const featuredPup = puppies.find(pup => pup.id === featPupId)
+  console.log(featuredPup)
   return (
     <>
       <div>
         {puppies.map(puppy => (
-          <p key={puppy.id}>{puppy.name}</p>
+          <p onClick={() => handleClick(puppy.id)} key={puppy.id}>
+            {puppy.name}
+          </p>
         ))}
+
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
       </div>
     </>
   )
